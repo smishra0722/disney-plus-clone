f6f5c9ff0682165d5f591e1670fcc6bdefc527e5
Shivam Mishra <shivam@Shivams-iMac.local>
2021-05-02 22:28:16 +0530

    initial build of clone

---
+++ src/components/Header.jsx
@@ -0,0 +1,103 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = () => {
+  return (
+    <Nav>
+      <Logo src='/images/logo.svg' />
+      <NavMenu>
+        <a>
+          <img src='/images/home-icon.svg' />
+          <span>Home</span>
+        </a>
+        <a>
+          <img src='/images/search-icon.svg' />
+          <span>Search</span>
+        </a>
+        <a>
+          <img src='/images/watchlist-icon.svg' />
+          <span>Watchlist</span>
+        </a>
+        <a>
+          <img src='/images/original-icon.svg' />
+          <span>Originals</span>
+        </a>
+        <a>
+          <img src='/images/movie-icon.svg' />
+          <span>Movies</span>
+        </a>
+        <a>
+          <img src='/images/series-icon.svg' />
+          <span>Series</span>
+        </a>
+      </NavMenu>
+      <UserImage />
+    </Nav>
+  )
+}
+
+export default Header
+
+const Nav = styled.nav`
+  display: flex;
+  align-items: center;
+  padding: 0 36px;
+  height: 70px;
+  background-color: #090b13;
+`
+
+const Logo = styled.img`
+  width: 80px;
+`
+
+const NavMenu = styled.div`
+  display: flex;
+  flex: 1;
+  margin-left: 25px;
+  align-items: center;
+  a {
+    display: flex;
+    align-items: center;
+    padding: 0 12px;
+    cursor: pointer;
+
+    img {
+      height: 20px;
+    }
+
+    span {
+      font-size: 13px;
+      letter-spacing: 1.42px;
+      position: relative;
+
+      &::after {
+        content: '';
+        height: 2px;
+        background: white;
+
+        position: absolute;
+        opacity: 0;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.45, 0.94);
+        transform: scaleX(0);
+      }
+
+      &:hover {
+        &::after {
+          opacity: 1;
+          transform: scaleX(1);
+        }
+      }
+    }
+  }
+`
+
+const UserImage = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+  background-color: red;
+`
